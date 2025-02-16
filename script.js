257d9ae6a1b604b0aabc3eb280386987214a8ce7
Cuceron <cuc3ron@yandex.ru>
2025-02-16 19:30:56 +0300

    space

---
+++ script.js
@@ -0,0 +1,7 @@
+// В данном примере JavaScript не обязателен, но если нужно добавить интерактивность, можно использовать его.
+// Например, для управления воспроизведением видео или анимациями.
+
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('bg-video');
+    video.play();
+});
